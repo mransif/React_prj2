0cd233997f052d0155f584d0c5ca3c313a388ed5
Kenz8489 <stark858981@gmail.com>
2025-02-24 20:50:07 +0530

    git

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,12 @@ function Footer() {
             <div className="text-[10vw] font-bold tracking-tight leading-none">
                 <h1>EYE</h1>
                 <h1 className='-mt-[2vw] leading-none'>OPENING</h1>
+                <div className="flex justify-around px-8 py-2  border-t-[1px]  border-zinc-800">
+            <a href="https://instagram.com/">Instagram</a>
+            <a href="https://linkedin.com/">LinkedIn</a>
+            <a href="https://x.com/">Twitter</a>
+            <a href="https://gmail.com/">Email</a>
+        </div>
             </div>
         </div>
         <div className="flex justify-around px-8 py-2  border-t-[1px]  border-zinc-800">

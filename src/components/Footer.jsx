df5dc8bd5664f067140267e0a369766b3bb8f128
Kenz8489 <stark858981@gmail.com>
2025-02-25 22:34:18 +0530

    git

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,8 +11,7 @@ function Footer() {
             </div>
         </div>
         <div className="flex justify-around px-8 py-2  border-t-[1px]  border-zinc-800">
-            <a href="https://instagram.com/">Instagram</a>
-            <a href="https://linkedin.com/">LinkedIn</a>
+            
             <a href="https://x.com/">Twitter</a>
             <a href="https://gmail.com/">Email</a>
         </div>
